f1b433371d2f3dee1f0351bc812ec0919966dc47
FaresYch <fares.ych16@gmail.com>
2025-04-17 00:15:25 +0200

    Rien

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	int num1, num2;
+	char op;
+	bool nonvalide = true;
+
+	while (nonvalide){
+		printf("============================================\n");
+
+        printf("Entrez num1 : ");
+        scanf("%d", &num1);
+		printf("\n");
+
+		printf("Entrez num2 : ");
+        scanf("%d", &num2);
+        printf("\n");
+
+        printf("Entrez une operation '+', '-', '*', '/' ");
+        scanf(" %c", &op);
+        printf("\n");
+		
+		switch (op){
+			case '+':
+				printf("%d + %d = %d", num1, num2, num1+num2);
+				nonvalide=false;
+				break;
+
+			case '-':
+				printf("%d - %d = %d", num1, num2, num1-num2);
+				nonvalide=false;
+				break;
+
+			case '*':
+				printf("%d * %d = %d", num1, num2, num1*num2);
+				nonvalide=false;
+				break;
+
+			case '/':
+				printf("%d / %d = %d", num1, num2, num1/num2);
+				nonvalide=false;
+				break;
+
+			default:
+				nonvalide = true;
+		}
+		printf("\n============================================\n");
+    }
+    return 0;
+}
 
